b0dac4a232d046b0d62c4daaa2d10889909e80ce
fernanda rodrigues <lucase.o.melo14@gmail.com>
2021-09-20 16:16:25 -0300

    segundo commit

---
+++ src/layout/Header.jsx
@@ -0,0 +1,9 @@
+import './Card.css'
+import React from 'react'
+
+//eslint-disable-next-line
+export default props => {
+    <div className="Header">
+        <h3>Projeto GO Group</h3>
+    </div>
+}
